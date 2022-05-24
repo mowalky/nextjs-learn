13422a8aae45da4e6069975f59f9b3e6637be74c
Mowalky <mowalky@outlook.com>
2022-05-24 06:49:32 -0500

    links

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
+import Link from "next/link";
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      Home
+      <ul>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/clients">Clients</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Home;
